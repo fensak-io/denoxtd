548360613185b3d07ca63edb6fc60ef606990ef5
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-26 13:46:46 -0500

    feat: Port utilities implemented in fensak app to here so it is available across more repos (#2)
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

---
+++ random/mod.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Fensak, LLC.
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ * random
+ * Contains utilities and functions for generating random data.
+ */
+export * from "./random.ts";
